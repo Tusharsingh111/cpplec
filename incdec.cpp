2a4721a9e7c85f956f08d21c4f1a039a05b3c0a8
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2024-06-04 16:16:07 +0530

    Add files via upload

---
+++ incdec.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 9;
+    cout<<n<<endl;
+
+    n++;
+    cout<<n<<endl;
+    ++n;
+    cout<<n<<endl;
+    --n;
+    cout<<n<<endl;
+    n--;
+    cout<<n<<endl;
+
+}
