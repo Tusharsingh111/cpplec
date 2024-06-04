d025f46c3ec2bda2e0b4d0396f48a8b46114851c
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2024-06-04 16:16:44 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 5;
+    int x = 6;
+    cout<<n<<endl;
+    n=9;
+    cout<<n<<endl;
+    n=n+10;
+    cout<<n<<endl;
+    n=n*4;
+    cout<<n<<endl;
+    n++;
+    cout<<n<<endl;
+    ++n;
+    cout<<n<<endl;
+    char ch = 'a';
+    int a = (int)ch;
+    cout<<a;
+
+}
