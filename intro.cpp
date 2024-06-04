145a3f6e17b0522aecdaa53f0b80aa92667000f6
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2024-06-04 16:16:26 +0530

    Add files via upload

---
+++ intro.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int n;
+    // cout<<"Enter the number N : "<<endl;
+    // cin>>n;
+    // cout<<"n="<<n;
+    float n = 5;
+    cout<<n;
+    return 0;
+}
