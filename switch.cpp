85fe3ae52f489243156d0a96571bc4d4ccc2fce3
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2024-06-13 20:49:35 +0530

    Add files via upload

---
+++ switch.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter month name";
+    cin>>num;
+    switch(num){
+        case 1:
+        cout<<"JANUARY";
+        break;
+        case 2:
+        cout<<"February";
+        break;
+        case 3:
+        cout<<"MARCH";
+        break;
+        case 4:
+        cout<<"April";
+        break;
+        case 5:
+        cout<<"May";
+        break;
+        case 6:
+        cout<<"June";
+        break;
+        case 7:
+        cout<<"July";
+        break;
+        case 8:
+        cout<<"August";
+        break;
+        case 9:
+        cout<<"September";
+        break;
+        case 10:
+        cout<<"October";
+        break;
+        case 11:
+        cout<<"November";
+        break;
+        case 12:
+        cout<<"December";
+        break;
+        default:
+        cout<<"NOT A MNTH";
+    }
+    return 0;
+}
