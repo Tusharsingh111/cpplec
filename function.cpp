d06ba73e3ffcdd47ffbea030a2d4bcc0d647fc37
Tushar Singh <115404905+Tusharsingh111@users.noreply.github.com>
2024-06-14 15:40:25 +0530

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void greet(){
+    cout<<"HELLO TUSHAR CHAUHAN"<<endl;
+}
+void city(){
+    cout<<"YOU ARE IN INDIA";
+}
+int main(){
+    greet();
+    city();
+}
